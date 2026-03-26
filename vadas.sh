GeorgeSapkin/vadas

commit 19bf93c98e94414472c81f673800ade213983d8b

cli: add connect command
Add dedicated connect command that's separate from start and don't offer
connecting on start anymore.

Remove the need for the user to press Enter on connect.

Signed-off-by: George Sapkin <george@sapk.in>

vadas.sh
@@ -71,6 +71,13 @@ function _print_help() {
 		  vm [<vm_name>]  Configure VM's network
 		EOF
 		;;
+	connect)
+		cat <<-EOF
+		[<vm_name>]
+
+		Connect to the console of a running VM.
+		EOF
+		;;
 	create)
 		cat <<-EOF
 		<subcommand>
@@ -137,7 +144,7 @@ function _print_help() {
 		cat <<-EOF
 		[<vm_name>]
 
-		Start a VM and connect to its console.
+		Start a VM.
 		EOF
 		;;
 	stop)
@@ -161,10 +168,11 @@ function _print_help() {
 		  clean           Clean up resources (e.g., 'temp')
 
 		Resource management:
-		  start           Start a VM and connect to it
+		  start           Start a VM
 		  stop|kill       Stop a running VM (use --force for immediate stop)
 		  pause|suspend   Pause a running VM
 		  resume          Resume a paused VM
+		  connect         Connect to the console of a running VM
 		  cp              Copy files and directories to and from a VM
 		  ps              List running VMs (--all includes paused VMs)
 		  show            Show resource details (e.g., 'ip')
@@ -256,19 +264,49 @@ function _confirm_overwrite() {
 }
 
 function _connect_to_vm() {
+	_ensure expect
 	_ensure virsh
+
+	local quiet=0
+	if [ "$1" == '--quiet' ]; then
+		quiet=1
+		shift
+	fi
 
 	local vm_name="$1"
 	local boot_wait="${2:-0}"
 
-	if _confirm "Connect to console of '$vm_name'?"; then
+	if [ "$quiet" -eq 1 ] || _confirm "Connect to console of '$vm_name'?"; then
 		stty sane
-		echo 'Please press Enter to activate this console after connecting.'
-		if [ "$boot_wait" -ne 0 ]; then
-			_countdown "$boot_wait" \
-				'Waiting for VM to boot to avoid mangled console output...'
+		if [ "$quiet" -eq 0 ]; then
+			if [ "$boot_wait" -ne 0 ]; then
+				_countdown "$boot_wait" \
+					'Waiting for VM to boot to avoid mangled console output...'
+			fi
 		fi
-		virsh console "$vm_name"
+
+		# Here-doc breaks interact
+		expect -c "
+		set timeout 5
+		spawn virsh console $vm_name
+
+		expect {
+			\"Connected to domain\" { exp_continue }
+			\"Escape character is\" {
+				sleep 0.5
+				send \"\r\"
+			}
+			timeout { exit 1 }
+		}
+
+		expect {
+			-re \"root@.*#\" {}
+			timeout { exit 1 }
+		}
+
+		set timeout -1
+		interact
+		"
 	fi
 }
 
@@ -700,7 +738,7 @@ function _create_vm() {
 			fi
 		fi
 
-		cmd_start "$vm_name" --no-connect
+		cmd_start "$vm_name"
 
 		if _confirm "Configure network for '$vm_name'?" y; then
 			sub_cmd_configure_vm "$vm_name" "$boot_wait"
@@ -1400,6 +1438,31 @@ function cmd_configure() {
 	esac
 }
 
+function cmd_connect() {
+	_ensure virsh
+
+	local vm_name="$1"
+
+	case "$vm_name" in
+	--help|-h)
+		_print_help connect
+		exit 0
+		;;
+	esac
+
+	if [ -z "$vm_name" ]; then
+		vm_name=$(_select_vm --state-running)
+		[ $? -ne 0 ] && exit 0
+	fi
+
+	if [ "$(_get_vm_state "$vm_name")" != 'running' ]; then
+		echo "${F_RED}ERROR${F_RESET}: VM '$vm_name' is not running." >&2
+		exit 1
+	fi
+
+	_connect_to_vm --quiet "$vm_name" 0
+}
+
 function cmd_cp() {
 	_ensure scp
 	_ensure virsh
@@ -1632,16 +1695,11 @@ function cmd_start() {
 	_ensure virsh
 
 	local vm_name
-	local connect=1
-
 	for arg in "$@"; do
 		case "$arg" in
 		--help|-h)
 			_print_help start
 			exit 0
-			;;
-		--no-connect)
-			connect=0
 			;;
 		*)
 			if [ -z "$vm_name" ]; then
@@ -1666,18 +1724,14 @@ function cmd_start() {
 	'shut off')
 		cmd=start
 		;;
-	*)
-		;;
 	esac
 
-	if [ -n "$cmd" ] && ! virsh "$cmd" "$vm_name" | _trim_empty; then
-		echo "Failed to $cmd '$vm_name'."
+	if [ -z "$cmd" ]; then
+		echo "'$vm_name' is $state."
 		exit 1
 	fi
 
-	if [ "$connect" -eq 1 ]; then
-		_connect_to_vm "$vm_name"
-	fi
+	virsh "$cmd" "$vm_name" | _trim_empty
 }
 
 function cmd_stop() {
@@ -2179,6 +2233,7 @@ function sub_cmd_show_ip() {
 
 case "${1:-}" in
 clean)           cmd_clean "$2" ;;
+connect)         cmd_connect "${@:2}" ;;
 configure)       cmd_configure "${@:2}" ;;
 cp|copy)         cmd_cp "${@:2}" ;;
 create)          cmd_create "$2" ;;

vadas_completion.sh
@@ -199,6 +199,7 @@ function _vadas_sh_completion() {
 	opts='
 		clean
 		configure
+		connect
 		cp
 		create
 		env
@@ -232,6 +233,10 @@ function _vadas_sh_completion() {
 			;;
 		configure)
 			COMPREPLY=( $(compgen -W "${configure_opts} ${help_opts}" -- "${cur}") )
+			return 0
+			;;
+		connect)
+			_comp_vms --state-running "${help_opts}"
 			return 0
 			;;
 		create)

commit 69b36de15f9eaa477d34d7f9d5eeed27c9e7295c

cli: only list stopped or paused VMs for start
Since start no longer connects to a VM, don't list started VMs.

Autocomplete paused VMs for stop.

Fix listing VMs when shutoff is one of the states.

Fixed: a38a8b7 ("cli: add initial version")
Fixes: 19bf93c ("cli: add connect command")
Signed-off-by: George Sapkin <george@sapk.in>

vadas.sh
@@ -1141,8 +1141,13 @@ function _get_vm_list() {
 
 	local state_flags
 	IFS=' ' read -r -a state_flags <<< "$@"
+
+	# virsh list can't handle multiple states when shutoff is specified
 	local vms
-	vms=$(virsh list "${state_flags[@]}" --name | _trim_empty)
+	for state in "${state_flags[@]}"; do
+		vms+=$(virsh list "$state" --name 2>/dev/null)
+		vms+=' '
+	done
 
 	for vm in $vms; do
 		if virsh metadata "$vm" --uri urn:vadas 2>/dev/null |
@@ -1729,7 +1734,7 @@ function cmd_start() {
 	done
 
 	if [ -z "$vm_name" ]; then
-		vm_name=$(_select_vm --all)
+		vm_name=$(_select_vm --state-paused --state-shutoff)
 		[ $? -ne 0 ] && exit 0
 	fi
 

vadas_completion.sh
@@ -156,17 +156,30 @@ function _comp_remote_path() {
 }
 
 function _comp_vms() {
-	local state_flag="$1"
-	local extra_opts="$2"
+	local virsh_flags=()
+	local extra_opts=()
+	for arg in "$@"; do
+		if [[ "$arg" == --state-* || "$arg" == "--all" ]]; then
+			virsh_flags+=("$arg")
+		else
+			extra_opts+=("$arg")
+		fi
+	done
+
 	local vm_ids
-	vm_ids=$(_get_vm_list "$state_flag")
-	COMPREPLY=( $(compgen -W "${vm_ids} ${extra_opts}" -- "${cur}") )
+	vm_ids=$(_get_vm_list "${virsh_flags[@]}")
+	COMPREPLY=( $(compgen -W "${vm_ids} ${extra_opts[*]}" -- "${cur}") )
 }
 
 function _get_vm_list() {
-	local state_flags="$*"
+	local state_flags=("$@")
+
+	# virsh list can't handle multiple states when shutoff is specified
 	local vms
-	vms=$(virsh list $state_flags --name 2>/dev/null)
+	for state in "${state_flags[@]}"; do
+		vms+=$(virsh list "$state" --name 2>/dev/null)
+		vms+=' '
+	done
 
 	for vm in $vms; do
 		if virsh metadata "$vm" --uri urn:vadas 2>/dev/null |
@@ -281,11 +294,11 @@ function _vadas_sh_completion() {
 				return 0
 				;;
 		start)
-			_comp_vms --all "${help_opts}"
+			_comp_vms --state-paused --state-shutoff "${help_opts}"
 			return 0
 			;;
 		stop|kill)
-			_comp_vms --state-running "${stop_opts} ${help_opts}"
+			_comp_vms --state-paused --state-running "${stop_opts} ${help_opts}"
 			return 0
 			;;
 		--force)